e88b9166465b8c8b612765c86101bb189bef5468
Khalel Abaquin <kabaquin694@west-mec.org>
2019-02-06 15:09:25 -0700

    First Update
    added name & date, changed 2 file names & commented out example code

--- tny_script.js
+++ tny_script.js
@@ -1,11 +1,13 @@
+"use strict";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Tutorial Case
 
    Countdown Clock
-   Author: 
-   Date:   
+   Author: Khalel Abaquin
+   Date:   2.5.19
 
 */
 
+// window.alert("Welcome\n to Tulsa");
